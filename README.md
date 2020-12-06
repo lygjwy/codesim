917033b7082c6d583614b7273bebb5d53042225d
lygjwy <lygjwy@qq.com>
2020-12-06 20:12:38 +0800

    improve code-similarity algorithm & complete artifact

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # codesim
 project codesim for course icse
+## Introduction
+A simple tool for evaluating code similarity
+## Environment
+- *nix System
+  - gcc
+  - objdump
+- python3
+## Run
+```
+./codesim.py code1 code2
+```
+## Contribution
+Pr
+
+
--- codesim.py
+++ codesim.py
@@ -18,28 +18,35 @@ def check_file(file_path):
         return False
     return True
 
-def get_hex_list_from_compile_file(file_path):
+def get_hex_list_from_compile_file(file_path, verbose=False):
     """
     compile code and return hexadecimal list
     """
     temp_file_path = tempfile.mkstemp(suffix='.o', dir='./')[1]
     # print("Temp File Path: {}".format(temp_file_path))
     subprocess.call(['gcc', '-c', '-o', temp_file_path, file_path])
-    p = subprocess.Popen(['objdump', '-s', '-j', '.text', temp_file_path], stdout=subprocess.PIPE)
+    p = subprocess.Popen(['objdump', '-d', temp_file_path], stdout=subprocess.PIPE)
     output = p.communicate()[0].decode("utf-8")
     # remove temp file
     subprocess.call(['rm', temp_file_path])
-    hex_pattern =  r'[0-9a-f]{8}'
-    return re.findall(hex_pattern, output)
+    # match assembly command related hex-code
+    hex_pattern =  r':\t(([0-9a-f]{2}\s){1,7})'
+    hex_list = []
+    for match in re.findall(hex_pattern, output):
+        hex_list.append(match[0].replace(' ', ''))
+    if verbose:
+        print("Compiled Code Assembly Commands Hex List: %r" % hex_list)
+    return hex_list
 
-def check_similarity(source_hex_list, target_hex_list):
+
+def check_similarity(source_hex_list, target_hex_list, verbose=False):
     """
     calculate similarity between source_hex_list and target_hex_list
     """
-    # print(source_hex_list)
-    # print(target_hex_list)
     match_hex_list = [hex for hex in source_hex_list if hex in target_hex_list]
-    similarity = float(len(match_hex_list)) / min(len(source_hex_list), len(target_hex_list))
+    if verbose:
+        print("Match Hex List: %r" % match_hex_list)
+    similarity = float(len(match_hex_list)) * 2 / (len(source_hex_list) + len(target_hex_list))
     return similarity
 
 
@@ -55,22 +62,16 @@ if __name__ == '__main__':
     source_file_path = args.code1
     target_file_path = args.code2
 
-    # check files path
+    verbose = args.verbose
+
+    # check files
     if check_file(source_file_path) or check_file(target_file_path):
         sys.stderr.write("ERROR: INVALID FILE PATH, CHECK PATH SPELLING AND FILE SUFFIX.\n")
         exit(-1)
 
     # compile file and get hex tuple
-    source_hex_list = get_hex_list_from_compile_file(source_file_path)
-    target_hex_list = get_hex_list_from_compile_file(target_file_path)
+    source_hex_list = get_hex_list_from_compile_file(source_file_path, verbose)
+    target_hex_list = get_hex_list_from_compile_file(target_file_path, verbose)
     
-    similarity = check_similarity(source_hex_list, target_hex_list)
+    similarity = check_similarity(source_hex_list, target_hex_list, verbose)
     print("{:.1f}".format(similarity*100))
-    # calculate similarity
-    # if args.verbose:
-    #     # debug mode
-    #     pass
-    # else:
-    #     # normal mode
-    #     pass
-    # print("End.")
